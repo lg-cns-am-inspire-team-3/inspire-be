f4c41e3d0f3931cfa700d71c5585e8c1502ec2b4
rymph <rymph0501@gmail.com>
2026-02-04 17:34:55 +0900

    first commit

---
+++ src/main/java/com/inspire/inspirebe/InspireBeApplication.java
@@ -0,0 +1,13 @@
+package com.inspire.inspirebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspireBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspireBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inspire/inspirebe/InspireBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspire.inspirebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspireBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
