c8794ad8d985b808ac51e5eaa5030786b5d3d3dd
choiseungjun <87286979+seungjun0209@users.noreply.github.com>
2026-02-11 04:21:57 +0900

    feat/controller and service, create DTO

--- src/main/java/com/inspire/inspirebe/attend/controller/AttendController.java
+++ src/main/java/com/inspire/inspirebe/attend/controller/AttendController.java
@@ -1,7 +1,13 @@
 package com.inspire.inspirebe.attend.controller;
 
+import com.inspire.inspirebe.attend.dto.AttendRequestDTO;
 import com.inspire.inspirebe.attend.service.AttendService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/attend")
 @RequiredArgsConstructor
 public class AttendController {
-    private final AttendService attendService;
+
+    private final AttendService attendService; 
+
+    
+    @PostMapping("/check")
+    public ResponseEntity<Void> attendCheck(
+            @AuthenticationPrincipal Object principal, // 현재 요청을 보낸 로그인된 사용자 정보를 SecurityContext에서 꺼내서 이 파라미터에 넣기
+            @RequestBody AttendRequestDTO request
+    ) {
+        /*
+         * AttendService에서 처리할 내용
+         * 1. QR 토큰 유효성 검증
+         * 2. 로그인 사용자 출석 가능 여부 확인
+         * 3. 중복 출석 방지
+         * 4. 출석 기록 저장
+         */
+
+        // attendService.attend(principal, request.getQrToken());
+
+        return ResponseEntity.noContent().build();
+    }
 }
