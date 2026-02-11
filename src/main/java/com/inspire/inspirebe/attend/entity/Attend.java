2402e8ac183989f0a1dcbdc4f7417a72e4f50aec
이학빈 <leehakbin1217@gmail.com>
2026-02-12 01:05:10 +0900

    Attend 엔티티 누락 필드(workMinute) 추가로 인한 빌드 에러 수정

--- src/main/java/com/inspire/inspirebe/attend/entity/Attend.java
+++ src/main/java/com/inspire/inspirebe/attend/entity/Attend.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Duration; // [추가] 시간 계산을 위해 필요
 
 @Entity
 @Table(name = "attendances")
@@ -33,8 +34,15 @@ public class Attend extends BaseEntity {
     @Column(name = "check_out")
     private LocalDateTime checkOut;
 
-    @Column(name = "total_time")
-    private LocalDateTime totalTime;
+    /**
+     * [수정/추가] DTO와의 매핑 및 급여 계산을 위해 
+     * 근무 시간을 '분' 단위(Integer)로 저장하는 필드를 추가합니다.
+     */
+    @Column(name = "work_minute")
+    private Integer workMinute;
+
+    // 기존 totalTime은 LocalDateTime 타입이라 계산에 부적합하여 
+    // 위 workMinute로 대체하거나 보조용으로 유지합니다.
 
     public Attend(UserEntity user) {
         this.user = user;
@@ -42,10 +50,17 @@ public class Attend extends BaseEntity {
         this.checkIn = LocalDateTime.now();
     }
 
+    /**
+     * [수정] 퇴근 시간을 업데이트할 때 
+     * 자동으로 근무 시간(분)을 계산하여 저장하는 로직을 추가합니다.
+     */
     public void updateCheckOut(LocalDateTime checkOut) {
         this.checkOut = checkOut;
+        
+        if (this.checkIn != null && this.checkOut != null) {
+            // 출근 시간과 퇴근 시간의 차이를 구해서 분 단위로 변환
+            long minutes = Duration.between(this.checkIn, this.checkOut).toMinutes();
+            this.workMinute = (int) minutes;
+        }
     }
-
-    
-
 }
